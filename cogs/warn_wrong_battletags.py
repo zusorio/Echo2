43bfc3430817dcce4990d8f63f8d971062eb6158
Tobias Messner <tobias.d.messner@gmail.com>
2020-04-14 18:45:31 +0200

    Made battletag warner suggest possible correct battletags, fixed issue where battletags with a lot of numbers were not detected properly, changed parts of warning messages

--- cogs/warn_wrong_battletags.py
+++ cogs/warn_wrong_battletags.py
@@ -4,6 +4,30 @@ from discord.ext import commands
 import helpers
 import pyowapi
 import re
+import aiohttp
+
+
+async def get_possible_correct_tag(wrong_battletag: str):
+    async with aiohttp.ClientSession() as session:
+        # If they messed up their capitalization this will return the correct capitalization
+        async with session.get(f"https://playoverwatch.com/en-us/search/account-by-name/{wrong_battletag.replace('#', '-')}") as r:
+            if r.status == 200:
+                profiles = await r.json()
+                # Filter the data to only include PC battletags
+                profiles = [profile for profile in profiles if profile["platform"] == "pc"]
+                # If we have one match that must be it
+                if len(profiles) == 1:
+                    return profiles[0]["name"]
+        # If they messed up their numbers as well see if their bnet name is unique
+        async with session.get(f"https://playoverwatch.com/en-us/search/account-by-name/{wrong_battletag.split('#')[0]}") as r:
+            if r.status == 200:
+                profiles = await r.json()
+                # Filter the data to only include PC battletags
+                profiles = [profile for profile in profiles if profile["platform"] == "pc"]
+                # If we have one match that must be it
+                if len(profiles) == 1:
+                    return profiles[0]["name"]
+        return False
 
 
 class WarnWrongBattletags(commands.Cog):
@@ -24,7 +48,7 @@ class WarnWrongBattletags(commands.Cog):
                 # Check if the message is in one of the bnet channels
                 if message.channel.id == channel["channel_id"]:
                     # Extract battletags
-                    battletags = re.findall("(?:\\S*)#(?:[0-9]{4,6})", message.content)
+                    battletags = re.findall("(?:\\S*)#(?:[0-9]{4,8})", message.content)
                     # Only check the message if it has exactly one battletag
                     if len(battletags) == 1:
                         try:
@@ -36,15 +60,21 @@ class WarnWrongBattletags(commands.Cog):
                             return
                         # If we can't get the player always warn them
                         if not player.success:
-                            await message.channel.send(
-                                f"{message.author.mention} I can't lookup your profile so lobby leaders may have problems inviting you!\n"
-                                f"Usually this is caused by a typo or incorrect CaPiTaLiZaTiOn of your battletag.")
-                            return
+                            correct_tag = await get_possible_correct_tag(player.bnet)
+                            if correct_tag:
+                                await message.channel.send(
+                                    f"{message.author.mention} That tag seems wrong! **Did you mean**: {correct_tag}")
+                                return
+                            else:
+                                await message.channel.send(
+                                    f"{message.author.mention} I can't lookup your profile so lobby leaders may have problems inviting you!\n"
+                                    f"Usually this is __caused by a typo or incorrect CaPiTaLiZaTiOn__ of your battletag.")
+                                return
                         # If the player has a private profile warn them if it's enabled for the current channel
                         if player.private and channel["public_required"]:
                             await message.channel.send(
                                 f"{message.author.mention} It looks like your profile is private. It needs to be public if you want to participate in pugs!\n"
-                                f"If your profile is already public disregard this, I can only see changes after a game restart.")
+                                f"If your profile is already public disregard this, __I can only see changes after a game restart.__")
                             return
                         # If the player is below level 25 warn them if it's enabled for the current channel
                         if player.actual_level < 25 and channel["level_25_required"]:

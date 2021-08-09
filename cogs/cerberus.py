6b8d445b175177cad3e1a43e5484f6b934068c71
Tobias Messner <tobias@zusor.io>
2021-08-09 13:33:10 +0200

    Add support for multiple guilds to cerberus

--- cogs/cerberus.py
+++ cogs/cerberus.py
@@ -22,11 +22,19 @@ class Cerberus(commands.Cog):
         self.log.info("Loaded Cog Cerberus")
         self.recent_messages = deque([], maxlen=100)
 
+    def get_guild_config(self, guild_id: int):
+        matching_guilds = [guild for guild in self.config.cerberus if guild["guild_id"] == guild_id]
+        if matching_guilds:
+            return matching_guilds[0]
+        else:
+            return False
+
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         self.log.info(f"Account {member.name}#{member.discriminator} created at {member.created_at}")
 
-        log_channel = self.bot.get_channel(self.config.cerberus["log_channel_id"])
+        guild_config = self.get_guild_config(member.guild.id)
+        log_channel = self.bot.get_channel(guild_config["log_channel_id"])
         account_age_text = humanfriendly.format_timespan(datetime.utcnow() - member.created_at)
 
         # Handle spam bots
@@ -40,7 +48,7 @@ class Cerberus(commands.Cog):
                              icon_url="https://cdn.discordapp.com/app-icons/581523092363411493/9f85d39eb6321ad12b2d13396c4595f5.png?size=256")
             await log_channel.send(embed=embed)
 
-        elif datetime.utcnow() - member.created_at < timedelta(seconds=self.config.cerberus["warn_max_time_seconds"]):
+        elif datetime.utcnow() - member.created_at < timedelta(seconds=guild_config["warn_max_time_seconds"]):
             embed = discord.Embed(title=f"Found suspicious new Member {member.name}#{member.discriminator}",
                                   description=f"Their account was created {account_age_text} ago", color=0x358BBB)
             embed.add_field(name="ID", value=member.id, inline=True)
@@ -61,7 +69,9 @@ class Cerberus(commands.Cog):
         is_mod = message.author.guild_permissions.manage_messages
         if similar_message_count > 5:
             account_age_text = humanfriendly.format_timespan(datetime.utcnow() - message.author.created_at)
-            log_channel = self.bot.get_channel(self.config.cerberus["log_channel_id"])
+
+            guild_config = self.get_guild_config(message.guild.id)
+            log_channel = self.bot.get_channel(guild_config["log_channel_id"])
 
             regex = r"\b((?:https?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b"
             urls_in_message = re.findall(regex, message.content)

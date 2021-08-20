c34951abad0d9055a57224ad437d118b9fdda91b
Toby <tobias.d.messner@gmail.com>
2021-08-20 14:08:48 +0200

    Temp disable PUG Points

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import logging
 from discord_handler import DiscordHandler
 from cogs import clean_old_messages, ping_for_messages, disable_reacts, vet_ping_unofficials, purge_channel, \
     reacts_required, delete_match_regex, initialize, warn_wrong_battletags, auto_question_answer, announce, \
-    alert_unofficial_start, most_active, jts_bnets, jts_lock, pug_points, help, cerberus, coached_timer
+    alert_unofficial_start, most_active, jts_bnets, jts_lock, help, cerberus, coached_timer
 
 sentry_sdk.init(
     "https://e5ceb20bb7f14c9bb04dcd723b83c559@o480512.ingest.sentry.io/5527831",
@@ -68,7 +68,7 @@ def main():
     bot.add_cog(most_active.MostActive(bot, config, log, credentials.airtable_api_key))
     bot.add_cog(jts_bnets.JTSBnets(bot, config, log))
     bot.add_cog(jts_lock.JTSLock(bot, config, log))
-    bot.add_cog(pug_points.PugPoints(bot, config, log, credentials.airtable_api_key))
+    # bot.add_cog(pug_points.PugPoints(bot, config, log, credentials.airtable_api_key))
     bot.add_cog(cerberus.Cerberus(bot, config, log))
     bot.add_cog(coached_timer.CoachedTimer(bot, config, log))
     bot.add_cog(help.Help(bot, config, log))
